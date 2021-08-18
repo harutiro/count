7ec119b66b97cda2ba0bfe12a091d07cbb939562
harutiro <harutiro2027@outlook.jp>
2021-08-19 02:20:54 +0900

    add: 初めてのコミット

---
+++ Rakefile
@@ -0,0 +1,9 @@
+require 'sinatra/activerecord'
+require 'sinatra/activerecord/rake'
+require './models/count.rb'
+
+namespace :db do
+    task :load_config do
+      require "./app"
+    end
+  end
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models/count.rb'
+
+before do
+  if Count.all.size == 0
+    Count.create(number: 0)
+  end
+end
+
+get '/' do
+  'こんばんはー'
+end
+
+get '/count' do
+  "カウントアプリ"
+end
